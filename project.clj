6659fadac47d11062e511071f228b987d65e32b6
Marcin Skotniczny <marcin.skotniczny@webservice.pl>
2013-07-11 22:45:23 +0200

    Added initially generated code

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject causeway "0.1.0-SNAPSHOT"
+  :description "Simple library for quick web development with Clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
