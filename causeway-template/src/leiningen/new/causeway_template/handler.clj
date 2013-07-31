efbf509d47d040542b58c46cd651a0494eea11e6
Marcin Skotniczny <cosmi.cosmi@gmail.com>
2013-07-31 22:26:03 +0200

    Update template

--- causeway-template/src/leiningen/new/causeway_template/handler.clj
+++ causeway-template/src/leiningen/new/causeway_template/handler.clj
@@ -3,23 +3,30 @@
         [causeway.bootconfig :only [devmode? bootconfig]]
         [compojure.route :only [not-found resources]]
         [compojure.core :only [context defroutes routes]]
-        [{{name}}.admin :only [admin-routes]]
         [causeway.util :only [routes-when]]
         [causeway.validation :only [wrap-validation]]
+        [causeway.assets]
         [{{name}}.auth :only [is-logged-in?]]
-        [{{name}}.app :only [public-routes logged-routes]])
+        [{{name}}.app :only [public-routes logged-routes]]
+        [{{name}}.admin :only [admin-routes]])
   (:require [compojure.handler :as handler]))
 
 
-
 (defroutes resource-routes
-  (->
-   (resource-handler
-    (combine-providers
-     (variant-provider "variants" "public")
-     (resource-provider "public")
-     ))))
-
+  (-> (combine-providers
+       ;; TODO: Remove if you don't use variants
+       (variant-provider "variants" "public")
+       (resource-provider "public"))
+      ;; TODO: Remove if you don't use coffeescript:
+      (wrap-processor (coffee-script-processor) "coffee" "js")
+      ;; TODO: Remove if you don't use lesscss:
+      (wrap-processor (less-css-processor) "less" "css")
+      (cond->
+       (not (devmode?))
+       (->
+        (wrap-processor (yui-css-compressor) "css" "css")
+        (wrap-processor (uglify-js-compressor) "js" "js")))
+      resource-handler))
 
 (def main-handler
   (-> 
@@ -30,7 +37,8 @@
      admin-routes
      resource-routes
      (not-found "Not Found"))
-   (wrap-variant-selector (constantly "en"))
+   ;; TODO: Add something like that:
+   ;; (wrap-variant-selector (constantly "en"))
    wrap-validation))
 
 (defn init []
