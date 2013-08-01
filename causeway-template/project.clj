8c171514524bdf54697e1e4f32a0540da570555b
Marcin Skotniczny <cosmi.cosmi@gmail.com>
2013-08-01 23:35:16 +0200

    template version

--- causeway-template/project.clj
+++ causeway-template/project.clj
@@ -1,7 +1,9 @@
-(defproject causeway-template/lein-template "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject causeway-template/lein-template "0.1.1b"
+  :description "Project template for using with causeway"
+  :url "https://github.com/cosmi/causeway"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+        :url "https://github.com/cosmi/causeway"})
