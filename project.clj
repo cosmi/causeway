56e0bcae6163da2192a88f9cc8210ba065c01689
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-05-18 17:07:48 +0200

    Update dependency versions

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject causeway "0.4.4"
+(defproject causeway "0.4.5"
   :description "Simple library for rapid web development with Clojure"
   :url "https://github.com/cosmi/causeway"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [sass "3.2.6"]
                  [digest "1.3.0"]
                  [ring "1.2.0"]
@@ -13,7 +13,7 @@
                  [clj-time "0.5.1"]
                  [org.marianoguerra/clj-rhino "0.2.1"]
                  [com.novemberain/monger "1.6.0"]
-                 [instaparse "1.2.7"]
+                 [instaparse "1.4.9"]
                  [org.clojure/core.match "0.2.0-rc5"]
                  [com.taoensso/timbre "2.6.1"] 
                  [ro.isdc.wro4j/wro4j-extensions "1.7.0"
